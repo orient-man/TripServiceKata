06fda8aead08eb597211bb7379d4b40e36298828
orientman <orientman@gmail.com>
2015-05-22 15:51:25 +0200

    Moving functionality into User class: should_inform_when_users_are_not_friends

---
+++ LegacyApp.Core.Tests/UserTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using LegacyApp.Core.Models;
+using NUnit.Framework;
+
+namespace LegacyApp.Core.Tests
+{
+    [TestFixture]
+    public class UserTests
+    {
+        private readonly User Bob = new UserBuilder().WithName("Bob").Build();
+        private readonly User Alice = new UserBuilder().WithName("Alice").Build();
+
+        [Test]
+        public void should_inform_when_users_are_not_friends()
+        {
+            // arrange
+            var user = new UserBuilder().FriendsWith(Bob).Build();
+
+            // act & assert
+            user.IsFriendWith(Alice).Should().BeFalse();
+        }
+    }
+}
