5881ca1a8396ab2e36118c906c29653e40669431
orientman <orientman@gmail.com>
2015-05-22 14:30:53 +0200

    2nd test: should_not_return_any_trips_when_users_are_not_friends

--- LegacyApp.Core.Tests/TripServiceTests.cs
+++ LegacyApp.Core.Tests/TripServiceTests.cs
@@ -12,6 +12,9 @@ namespace LegacyApp.Core.Tests
         private static User loggedInUser;
         private readonly User UnusedUser = null;
         private readonly User Guest = null;
+        private readonly User RegisteredUser = new User { Name = "Alice" };
+        private readonly User AnotherUser = new User { Name = "Bob" };
+        private readonly Trip ToBrazil = new Trip();
 
         [Test]
         public void should_throw_an_exception_when_not_logged_in()
@@ -25,6 +28,20 @@ namespace LegacyApp.Core.Tests
 
             // assert
             act.ShouldThrow<UserNotLoggedInException>();
+        }
+
+        [Test]
+        public void should_not_return_any_trips_when_users_are_not_friends()
+        {
+            // arrange
+            var service = new TestingTripService();
+            loggedInUser = RegisteredUser;
+            var friend = new User();
+            friend.Friends.Add(AnotherUser);
+            friend.Trips.Add(ToBrazil);
+
+            // act & assert
+            service.GetTripsByUser(friend).Should().BeEmpty();
         }
 
         class TestingTripService : TripService
