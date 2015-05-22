ab0af8bb436321195e7212bf5ae22d7da1a189f2
orientman <orientman@gmail.com>
2015-05-22 12:44:31 +0200

    Beautiful Legacy App :)

---
+++ LegacyApp.Core/Dao/UserDao.cs
@@ -0,0 +1,13 @@
+﻿using LegacyApp.Core.Models;
+
+namespace LegacyApp.Core.Dao
+{
+    public class UserDao
+    {
+        public static User GetByName(string name)
+        {
+            // stinking SQL query here
+            return new User { Name = name };
+        }
+    }
+}
---
+++ LegacyApp.Core/Models/User.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+
+namespace LegacyApp.Core.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+        public List<User> Friends { get; set; }
+
+        public List<Trip> Trips { get; set; }
+
+        public User()
+        {
+            Name = "";
+            Friends = new List<User>();
+            Trips = new List<Trip>();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj != null && Equals(obj as User);
+        }
+
+        public bool Equals(User u)
+        {
+            return u != null && u.Name == Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+    }
+}
