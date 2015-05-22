f6c5d9521994f6fb22ccda2abffc4c417ae59812
orientman <orientman@gmail.com>
2015-05-22 14:15:03 +0200

    First failing test for shortest branch: should_throw_an_exception_when_not_logged_in

---
+++ LegacyApp.Core.Tests/TripServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FluentAssertions;
+using LegacyApp.Core.Services;
+using NUnit.Framework;
+
+namespace LegacyApp.Core.Tests
+{
+    [TestFixture]
+    public class TripServiceTests
+    {
+        [Test]
+        public void should_throw_an_exception_when_not_logged_in()
+        {
+            // arrange
+            var service = new TripService();
+
+            // act
+            Action act = () => service.GetTripsByUser(null);
+
+            // assert
+            act.ShouldThrow<UserNotLoggedInException>();
+        }
+    }
+}
