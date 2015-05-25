de8d74e7b2c90a763f2ac9239ba396c2ff2e25fe
orientman <orientman@gmail.com>
2015-05-25 15:06:28 +0200

    Move from static call to proper constructor dependency

---
+++ LegacyApp.Core/Dao/ITripDao.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using LegacyApp.Core.Models;
+
+namespace LegacyApp.Core.Dao
+{
+    public interface ITripDao
+    {
+        List<Trip> FindTripsByUser(User user);
+    }
+}
